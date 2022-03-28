9793b6398e73fe4b96908572828a155df57d545c
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-28 12:54:01 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int currpoint = 0;
+int genome = {
+	'A',
+	'A',
+	'T',
+	'G',
+	'C',
+	'C'
+};
+
+void executeDNA() {
+	int codonbuff[3];
+	for(int i = 0; i < 3; i++;) {
+		printf("%d\n", i);
+		codonbuff[i] = genome[i];
+	}
+	for (int i = 0; i < 3; i++) {     
+        printf(codonbuff[i]);     
+    	}      
+}
+	
+
+int main() {
+	while(!stop) {
+		executeDNA();
+		currpoint++;
+	}
+	return 0;
+}
