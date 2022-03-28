41f5c4ba9b80b1a4f5ece99f39bab45ec0bd907a
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-28 13:28:05 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int currpoint = 0;
 int run = 1;
 // atcg
 
-char genome[16] = {
+char* genome[16] = {
 	"A",
 	"A",
 	"A"
@@ -15,8 +15,7 @@ void executeDNA() {
 	char* codonbuff[4];
 	codonbuff[3] = "\0";
 	for (int i = 0; i < 3; i++) {  
-		codonbuff[i] = genome[i];
-        	printf("%c", codonbuff[i]);     
+		codonbuff[i] = genome[i];  
     	}      
 	if(strcmp(codonbuff, "AAA") == 0) {
 		run = 0;
